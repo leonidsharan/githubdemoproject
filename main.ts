61c6abc1596cee4021f1b332c4d19b4a9c890316
leonidsharan <67856886+leonidsharan@users.noreply.github.com>
2022-07-15 10:32:08 +0300

    Restore '15.07.2022, 10:15:36 Button controle'

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(200)
+    counter += 1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
 basic.forever(function () {
 	
 })
