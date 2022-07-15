126dfaa67e79c121e429f06443330ec08ae2a596
leonidsharan <67856886+leonidsharan@users.noreply.github.com>
2022-07-15 10:26:47 +0300

    bad bad bad

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    basic.pause(200)
-    counter += 1
-    basic.showNumber(counter)
-})
-let counter = 0
-counter = 0
 basic.forever(function () {
 	
 })
